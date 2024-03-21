9b9fdfb7363778cc38bdb3ba0262428f262e2281
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-22 00:10:40 +0100

    Country Users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Script creates a table users, using 
+-- id, email, name, country columns
+CREATE TABLE IF NOT EXISTS users(
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255),
+       country ENUM('US', 'CO', 'TN') NOT NULL
+);
