5e058174e06f49ec150251792fef8dc1078dc8fe
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-23 22:11:27 +0100

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- Script creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
 -- Note: An average score can be a decimal
-DELIMITERS //
+DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
