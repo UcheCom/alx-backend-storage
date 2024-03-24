5cbcb683ba270a387b0d9786bf9fd0fcf89d116e
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-24 10:32:53 +0100

    Average weighted score fixed

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,18 +6,10 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-	SET weight_avg_score = (
-	    SELECT SUM(score * weight) / SUM(weight)
-	    FROM users AS u
-	    JOIN corrections AS c
-	    ON u.id = c.user_id
-	    JOIN projects AS p
-	    ON c.project_id = p.id
-	    WHERE u.id = user_id
-	);
-
 	UPDATE users 
-	SET average_score = weight_avg_score;
+	SET average_score = 
+	(SELECT SUM(p.weight * c.score) / SUM(p.weight) FROM projects p, corrections c WHERE c.user_id=user_id AND p.id=project_id)
+	WHERE users.id=user_id;
 END;
 //
 DELIMITER ;
