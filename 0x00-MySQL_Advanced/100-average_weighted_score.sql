d3bc5e75bf37dd45088065d7f31a7f4236ba2597
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-24 05:56:24 +0100

    Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,24 @@
+-- Script creates procedure ComputeAverageWeightedScoreForUser
+-- that computes and store the average weighted score for a student
+-- the procedure takes 1 input.
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	SET weight_avg_score = (
+	    SELECT SUM(score * weight) / SUM(weight)
+	    FROM users AS user
+	    JOIN corrections AS c
+	    ON user.id = c.user_id
+	    JOIN projects AS p
+	    ON c.project_id = p.id
+	    WHERE user.id = user_id
+	);
+
+	UPDATE users 
+	SET average_score = weight_average_score
+	WHERE id = user_id;
+END;
+//
+DELIMITER ;
