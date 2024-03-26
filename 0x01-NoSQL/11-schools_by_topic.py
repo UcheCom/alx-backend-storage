92937290996a3327ec17fda6743c79368b2fba62
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-27 00:20:54 +0100

    MongoDB file

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-Module returns the list of school having a specific topic
+"""Module returns the list of school having a specific topic"""
 
 
 def schools_by_topic(mongo_collection, topic):
