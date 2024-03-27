5eef08c558f51a107517060da8a3d2e8e279017e
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-27 10:18:34 +0100

    Log file fixed

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,14 +6,12 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     """Provides some stats about Nginx logs"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_coll = client.logs.nginx
-    print("{} logs".format(nginx_coll.est_document_count()))
+    nginx_col = client.logs.nginx
+    print("{} logs".format(nginx_col.est_document_count()))
     print("Methods:")
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for m in method:
-        print("\tmethod {}: {}".format(
-            m,
-            nginx_coll.count_documents({'method': m})
-
-    print("{} status check".format(
-        nginx_coll.count_documents({'method': 'GET', 'path': '/status'})))
+        count = nginx_col.count_documents({'method': m})
+        print("\tmethod {}: {}".format(method, count))
+    stat_get = nginx_col.count_documents({'method': 'GET', 'path': '/status'})
+    print("{} status check".format(stat_get))
