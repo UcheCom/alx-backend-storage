0cf3757c9d5537ff22aa0acfc8831bcdff0f06b9
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-29 22:29:29 +0100

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,34 @@ from typing import Union, Callable, Optional
 from functools import wraps
 
 
+def replay(method: Callable) -> None:
+    """This displays the history of calls of a particular function"""
+    rd = redis.Redis()
+    name = method.__qualname__
+    count = rd.get(name)
+    try:
+        count = int(count.decode("utf-8"))
+    except Exception:
+        count = 0
+
+    print("{} was called {} times:".format(name, count))
+    inputs = r.lrange("{}:inputs".format(name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(name), 0, -1)
+
+    for ip, ou in zip(inputs, outputs):
+        try:
+            ip = ip.decode("utf-8")
+        except Exception:
+            ip = ""
+
+        try:
+            ou = ou.decode("utf-8")
+        except Exception:
+            ou = ""
+
+        print("{}(*{}) -> {}".format(name, ip, ou))
+
+
 def call_history(method: Callable) -> Callable:
     """Stores the history of inputs and outputs for a particular function"""
     key_in = method.__qualname__ + ":inputs"
